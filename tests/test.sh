b68c700d2ff39c2bfb7167c776981c002a7f0d6c
Mathias157 <mathiasros@gmail.com>
2024-09-19 12:00:00 +0200

    Gave permission to use the shell script

--- tests/test.sh
+++ tests/test.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+rem Note, i could not make this work without this command locally:
+rem git update-index --chmod=+x tests/test.sh
+
 python src/software_title/script.py --a "A string" --b 5.7
