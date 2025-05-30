bba6beb6d46fc57b5fa82d6e8ab78190090b39b9
mathias157 <mathiasros@gmail.com>
2025-05-30 17:24:24 +0200

    Machine learning example with PyTorch

--- src/software_title/script.py
+++ src/software_title/script.py
@@ -1,22 +1,146 @@
-#!/usr/bin/env python
+"""
+Machine Learning with PyTorch
+
+An implementation of the tutorial in https://docs.pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html
+
+Created on 30.05.2025
+@author: Mathias Berg Rosendal, PhD Student at DTU Management (Energy Economics & Modelling)
+"""
+#%% ------------------------------- ###
+###        0. Script Settings       ###
+### ------------------------------- ###
+
+import matplotlib.pyplot as plt
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
 import click
 
+
+#%% ------------------------------- ###
+###          1. Functions           ###
+### ------------------------------- ###
+
+# Define model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+    
+    
+def train(dataloader, model, loss_fn, optimizer, device):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test(dataloader, model, loss_fn, device):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+#%% ------------------------------- ###
+###            2. Main              ###
+### ------------------------------- ###
+
 @click.command()
-@click.option("--a", type=str, required=True, help="A string")
-@click.option("--b", type=float, required=True, help="An integer")
-def func(a:str, b: float, c: float = 1.5):
-    """_summary_
+@click.option('--dark-style', is_flag=True, required=False, help='Dark plot style')
+def main(dark_style: bool):
 
-    Args:
-        a (str): _description_
-        b (int): _description_
-        c (float, optional): _description_. Defaults to 1.5.
+    # Set global style of plot
+    if dark_style:
+        plt.style.use('dark_background')
+        fc = 'none'
+    else:
+        fc = 'white'
 
-    Returns:
-        _type_: _description_
-    """
-    print(f'Input a is {a}, and the sum of b and c is {b+c}')
+    # Check device and make model
+    device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+    print(f"Using {device} device")
+    
+    model = NeuralNetwork().to(device)
+    print(model)
+
+    # Create loss function and optimizer
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+    # Load data
+    batch_size = 64
+
+    ## Download training data from open datasets.
+    training_data = datasets.FashionMNIST(
+        root="data",
+        train=True,
+        download=True,
+        transform=ToTensor(),
+    )
+
+    ## Download test data from open datasets.
+    test_data = datasets.FashionMNIST(
+        root="data",
+        train=False,
+        download=True,
+        transform=ToTensor(),
+    )
+
+    ## Create data loaders.
+    train_dataloader = DataLoader(training_data, batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+    for X, y in test_dataloader:
+        print(f"Shape of X [N, C, H, W]: {X.shape}")
+        print(f"Shape of y: {y.shape} {y.dtype}")
+        break
+    
+    # Train 
+    epochs = 5
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer, device)
+        test(test_dataloader, model, loss_fn, device)
+    print("Done!")
+
+    # Save model
+    torch.save(model.state_dict(), "model.pth")
+    print("Saved PyTorch Model State to model.pth")
 
 if __name__ == '__main__':
-    func()
-    
+    main()
